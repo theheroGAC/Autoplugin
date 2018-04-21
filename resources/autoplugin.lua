2875066430c98e43932766c3c3fe0a1023f8d2a2
theheroGAC <gamesandconsolestaff@gmail.com>
2018-04-21 15:11:52 +0200

    Add files via upload

--- resources/autoplugin.lua
+++ resources/autoplugin.lua
@@ -1,6 +1,7 @@
 ------Autoinstall pluign
 		files.extract("resources/autoplugin.zip","ur0:/tai")
-		files.extract("resources/config.zip","ur0:/tai")
+              files.extract("resources/autoplugin.zip","ux0:/tai")
+		files.extract("resources/config.zip","ux0:/tai")
 
 		os.message("Your PSVita will restart...\nThe plugins have been installed",0)
 		os.delay(2500)
