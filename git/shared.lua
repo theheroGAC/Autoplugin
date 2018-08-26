4614fa26da5530ad8ec9571ac1cd012ed8346311
theheroGAC <gamesandconsolestaff@gmail.com>
2018-08-26 11:52:49 +0200

    V3.22

--- git/shared.lua
+++ git/shared.lua
@@ -4,5 +4,5 @@ APP_REPO = "theheroGAC"
 APP_PROJECT = "Autoplugin"
 
 APP_VERSION_MAJOR = 0x03 -- major.minor
-APP_VERSION_MINOR = 0x21
+APP_VERSION_MINOR = 0x22
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
