30de229e4e0299baf3b726871bacdd777a4780b3
theheroGAC <gamesandconsolestaff@gmail.com>
2018-09-25 19:00:15 +0200

    Add files via upload

--- plugins/db.lua
+++ plugins/db.lua
@@ -4,40 +4,40 @@ db = {
 
 --Kernel
 { name = "NoNpDrm by TheOfficialFloW", path = "nonpdrm.skprx", version = "0001", stringversion = "1.2" },
-{ name = "NoPsmDrm by frangarcj", path = "nopsmdrm.skprx", version = "0002", stringversion = "1.3" },
-{ name = "Vitabright by devnoname120(only for 3.60/H-Encore 3.68)", path = "vitabright.skprx", version = "0003", stringversion = "0.9" },
-{ name = "NoAVLS by SilicaAndPina", path = "noavls.skprx", version = "0004", stringversion = "0.1" },
-{ name = "Repatch by dots-tb v2.71", path = "repatch.skprx", version = "0005", stringversion = "2.71" },
-{ name = "LOLIcon by dots-tb v1.0.1.1 HOTFIX A", path = "lolicon.skprx", version = "0006", stringversion = "1.0.1.1" },
-{ name = "ds3vita by xerpi Beta3", path = "ds3vita.skprx", version = "0007", stringversion = "0.3" },
-{ name = "ds4vita by xerpi v1.2", path = "ds4vita.skprx", version = "0008", stringversion = "1.2" },
-{ name = "udcd_uvc by xerpi v1.1", path = "udcd_uvc.skprx", version = "0009", stringversion = "1.1" },
---{ name = "Kuio by Rinnegatamante", path = "kuio.skprx", version = "0010", stringversion = "1.2" },
-{ name = "Adrenaline by TheOfficialFloW (Fix double touch)", path = "adrenaline_kernel.skprx", version = "0011", stringversion = "1.2" },
-{ name = "DSMotion by OperationNT414C v1.2", path = "dsmotion.skprx", version = "0012", stringversion = "1.2" },
-{ name = "usbmc by yifanlu v6 (only for PSTV)", path = "usbmc.skprx", version = "0013", stringversion = "0.6" },
-{ name = "AnalogsEnhancer by Rinnegatamante v1.0", path = "AnalogsEnhancer.skprx", version = "0014", stringversion = "1.0" },
-{ name = "ioplus by dots-tb v0.1", path = "ioplus.skprx", version = "0015", stringversion = "0.1" },
-{ name = "Vitacheat Z05 BETA by r0ah", path = "vitacheat.skprx", version = "0016", stringversion = "1.0" },
+{ name = "NoPsmDrm by frangarcj", path = "nopsmdrm.skprx", version = "0001", stringversion = "1.3" },
+{ name = "Vitabright by devnoname120(only for 3.60/H-Encore 3.68)", path = "vitabright.skprx", version = "0001", stringversion = "0.9" },
+{ name = "NoAVLS by SilicaAndPina", path = "noavls.skprx", version = "0001", stringversion = "0.1" },
+{ name = "Repatch by dots-tb v2.71", path = "repatch.skprx", version = "0001", stringversion = "2.71" },
+{ name = "LOLIcon by dots-tb v1.0.1.1 HOTFIX A", path = "lolicon.skprx", version = "0001", stringversion = "1.0.1.1" },
+{ name = "ds3vita by xerpi Beta3", path = "ds3vita.skprx", version = "0001", stringversion = "0.3" },
+{ name = "ds4vita by xerpi v1.2", path = "ds4vita.skprx", version = "0001", stringversion = "1.2" },
+{ name = "udcd_uvc by xerpi v1.1", path = "udcd_uvc.skprx", version = "0001", stringversion = "1.1" },
+--{ name = "Kuio by Rinnegatamante", path = "kuio.skprx", version = "0001", stringversion = "1.2" },
+{ name = "Adrenaline by TheOfficialFloW (Fix double touch)", path = "adrenaline_kernel.skprx", version = "0001", stringversion = "1.2" },
+{ name = "DSMotion by OperationNT414C v1.2", path = "dsmotion.skprx", version = "0001", stringversion = "1.2" },
+{ name = "usbmc by yifanlu v6 (only for PSTV)", path = "usbmc.skprx", version = "0001", stringversion = "0.6" },
+{ name = "AnalogsEnhancer by Rinnegatamante v1.0", path = "AnalogsEnhancer.skprx", version = "0001", stringversion = "1.0" },
+{ name = "ioplus by dots-tb v0.1", path = "ioplus.skprx", version = "0001", stringversion = "0.1" },
+{ name = "Vitacheat Z05 BETA by r0ah", path = "vitacheat.skprx", version = "0001", stringversion = "1.0" },
 
 --Boot_config.txt
-{ name = "Custom Boot Splash by Princess of Sleeping", path = "custom_boot_splash.skprx", version = "0017", stringversion = "1.0" },
+{ name = "Custom Boot Splash by Princess of Sleeping", path = "custom_boot_splash.skprx", version = "0001", stringversion = "1.0" },
 
 --Main
-{ name = "Download Enabler by TheOfficialFloW (VitaTweaks)", path = "download_enabler.suprx", version = "0018", stringversion = "0.4" },
-{ name = "NoLockScreen v2 by TheOfficialFloW (VitaTweaks)", path = "nolockscreen.suprx", version = "0019", stringversion = "0.2" },
-{ name = "NoTrophyMsg by TheOfficialFloW (VitaTweaks)", path = "notrophymsg.suprx", version = "0020", stringversion = "0.1" },
-{ name = "Custom Warning v2 by TheOfficialFloW (VitaTweaks)", path = "custom_warning.suprx", version = "0021", stringversion = "0.2" },
+{ name = "Download Enabler by TheOfficialFloW (VitaTweaks)", path = "download_enabler.suprx", version = "0001", stringversion = "0.4" },
+{ name = "NoLockScreen v2 by TheOfficialFloW (VitaTweaks)", path = "nolockscreen.suprx", version = "0001", stringversion = "0.2" },
+{ name = "NoTrophyMsg by TheOfficialFloW (VitaTweaks)", path = "notrophymsg.suprx", version = "0001", stringversion = "0.1" },
+{ name = "Custom Warning v2 by TheOfficialFloW (VitaTweaks)", path = "custom_warning.suprx", version = "0001", stringversion = "0.2" },
 
-{ name = "Shellbat by nowrep v0.9", path = "shellbat.suprx", version = "0022", stringversion = "0.9" },
-{ name = "Shellsecbat by OperationNT414C v0.9", path = "shellsecbat.suprx", version = "0023", stringversion = "0.9" },
-{ name = "pngshot by xyzz v1.2", path = "pngshot.suprx", section = "main",  version = "0024", stringversion = "1.2" },
-{ name = "Vflux by Applelo v0.5", path = "vflux.suprx", section = "main",  version = "0025", stringversion = "0.5" },
+{ name = "Shellbat by nowrep v0.9", path = "shellbat.suprx", version = "0001", stringversion = "0.9" },
+{ name = "Shellsecbat by OperationNT414C v0.9", path = "shellsecbat.suprx", version = "0001", stringversion = "0.9" },
+{ name = "pngshot by xyzz v1.2", path = "pngshot.suprx", section = "main",  version = "0001", stringversion = "1.2" },
+{ name = "Vflux by Applelo v0.5", path = "vflux.suprx", section = "main",  version = "0001", stringversion = "0.5" },
 
 --All
-{ name = "PSV-VSH MENU by joel16 v3.4", path = "vsh.suprx", version = "0026", stringversion = "3.4" },
-{ name = "VitaGrafix by Electry v2.4", path = "vitagrafix.suprx", version = "0027", stringversion = "2.4" },
-{ name = "Oclock Vita by frangarcj v1.2.1", path = "oclockvita.suprx", version = "0028", stringversion = "1.2.1" },
+{ name = "PSV-VSH MENU by joel16 v3.4", path = "vsh.suprx", version = "0001", stringversion = "3.4" },
+{ name = "VitaGrafix by Electry v2.4", path = "vitagrafix.suprx", version = "0001", stringversion = "2.4" },
+{ name = "Oclock Vita by frangarcj v1.2.1", path = "oclockvita.suprx", version = "0001", stringversion = "1.2.1" },
 
 
 
