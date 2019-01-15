343c5da05fc03de884b9a6d9e3e4a0c8311bf401
theheroGAC <gamesandconsolestaff@gmail.com>
2019-01-15 21:34:15 +0100

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,44 +1,28 @@
 --[[ 
 	Autoinstall plugin
-	
+
 	Licensed by Creative Commons Attribution-ShareAlike 4.0
 	http://creativecommons.org/licenses/by-sa/4.0/
 	
+	Dev: TheHeroeGAC
 	Designed By Gdljjrod & DevDavisNunez.
 	Collaborators: BaltazaR4 & Wzjk.
 ]]
 
---Show splash ...
-splash.zoom("resources/splash.png")
-back = image.load("resources/back.png")
+--Activamos Paleta de Colores Precargados
 color.loadpalette()
 
-local wstrength = wlan.strength()
-if wstrength then
-	if wstrength > 55 then dofile("git/updater.lua") end
-end
+--Show splash
+splash.zoom("resources/splash.png")
 
--- Loading language file
-files.mkdir("ux0:data/AUTOPLUGIN/lang/")
-dofile("lang/english_us.txt")
+--Imagen de Fondo
+back = image.load("resources/back.png")
+adr = image.load("resources/adr.png")
+psvita = image.load("resources/psvita.png")
 
-if files.exists("ux0:data/AUTOPLUGIN/lang/"..os.language()..".txt") then
-	dofile("ux0:data/AUTOPLUGIN/lang/"..os.language()..".txt")
-end
-if files.exists("lang/"..os.language()..".txt") then
-	dofile("lang/"..os.language()..".txt")
-end
-
--- Loading font
-files.mkdir("ux0:data/AUTOPLUGIN/font/")
-if files.exists("ux0:data/AUTOPLUGIN/font/font.ttf") then
-	fnt = font.load("ux0:data/AUTOPLUGIN/font/font.ttf")
-elseif files.exists("ux0:data/AUTOPLUGIN/font/font.pgf") then
-	fnt = font.load("ux0:data/AUTOPLUGIN/font/font.pgf")
-elseif files.exists("ux0:data/AUTOPLUGIN/font/font.pvf") then
-	fnt = font.load("ux0:data/AUTOPLUGIN/font/font.pvf")
-end
-if fnt then	font.setdefault(fnt) end
+--Sprites para Botones
+buttonskey = image.load("resources/buttons.png",20,20)
+buttonskey2 = image.load("resources/buttons2.png",30,20)
 
 if os.access() == 0 then
 	if back then back:blit(0,0) end
@@ -47,23 +31,45 @@ if os.access() == 0 then
 	os.exit()
 end
 
--- Utilities
-dofile("scripts/utilities/commons.lua")
-dofile("scripts/utilities/plugins.lua")
-dofile("scripts/utilities/tai.lua")
+--Updater
+if wlan.strength() > 55 then dofile("git/updater.lua") end
 
--- Main Menu
-dofile("scripts/menus/main/exit.lua")
-dofile("scripts/menus/main/installp.lua")
-dofile("scripts/menus/main/uninstallp.lua")
-dofile("scripts/menus/main/updatep.lua")
+-- Loading language file
+dofile("lang/english_us.txt")
 
--- Extras Menu
-dofile("scripts/menus/extras/customsplash.lua")
-dofile("scripts/menus/extras/menu.lua")
-dofile("scripts/menus/extras/npdrm.lua")
-dofile("scripts/menus/extras/pspctrls.lua")
-dofile("scripts/menus/extras/qencore.lua")
-dofile("scripts/menus/extras/sd2vita.lua")
+files.mkdir("ux0:data/AUTOPLUGIN/lang/")
+if files.exists("ux0:data/AUTOPLUGIN/lang/"..os.language()..".txt") then
+	dofile("ux0:data/AUTOPLUGIN/lang/"..os.language()..".txt")	
+end
+if files.exists("lang/"..os.language()..".txt") then
+	dofile("lang/"..os.language()..".txt")
+end
 
-dofile("scripts/menus/main/menu.lua")--Main Cycle
+-- Loading font
+files.mkdir("ux0:data/AUTOPLUGIN/font/")
+fnt = font.load("ux0:data/AUTOPLUGIN/font/font.pgf") or font.load("ux0:data/AUTOPLUGIN/font/font.pvf") or font.load("ux0:data/AUTOPLUGIN/font/font.ttf")
+if fnt then	font.setdefault(fnt) end
+
+dofile("scripts/scroll.lua")
+dofile("scripts/tai.lua")
+dofile("scripts/commons.lua")
+
+--Funciones PSVITA
+dofile("scripts/psvita/sd2vita.lua")
+dofile("scripts/psvita/plugins.lua")
+dofile("scripts/psvita/pmanager.lua")
+dofile("scripts/psvita/autoplugin.lua")
+dofile("scripts/psvita/onlinep.lua")
+dofile("scripts/psvita/menu_psvita.lua")
+
+--Funciones PSP
+dofile("scripts/psp/npdrm.lua")
+dofile("scripts/psp/pspctrls.lua")
+dofile("scripts/psp/menu_psp.lua")
+
+--Funciones EXTRAS
+dofile("scripts/extras/customsplash.lua")
+dofile("scripts/extras/menu_extras.lua")
+
+--Main Cycle
+dofile("menu.lua")
