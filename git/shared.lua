eeb92f89ee49c4c62df9c699e3d40cbf4005f686
theheroGAC <gamesandconsolestaff@gmail.com>
2019-12-21 20:25:09 +0100

    Add files via upload

--- git/shared.lua
+++ git/shared.lua
@@ -4,5 +4,5 @@ APP_REPO = "theheroGAC"
 APP_PROJECT = "Autoplugin"
 
 APP_VERSION_MAJOR = 0x04 -- major.minor
-APP_VERSION_MINOR = 0x11
+APP_VERSION_MINOR = 0x13
 APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
